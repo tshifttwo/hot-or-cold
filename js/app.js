187c677571463cf13b40e2b75c6db036a1dcf62b
tshifttwo <tatianac88@gmail.com>
2016-05-04 21:43:44 -0400

    guessed correct

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 
 $(document).ready(function(){
+
 	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
@@ -11,6 +12,40 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
+
+  	/*--- New Game button ---*/
+
+// function newgame() {
+//     return Math.floor((Math.random() * 100) + 1);
+/*onclick run random number again
+
+
+  	/*--- Pick Random Number ---*/
+
+	var randomnumber =  Math.floor((Math.random() * 100) + 1);
+    console.log("The secret number is: " + randomnumber);
+
+
+	/*--- Get Guess---*/
+
+
+    $("#form").on("submit", function(e){
+    	e.preventDefault();
+    	game();
+    });
+    	
+
+
+	/*--- you guessed it---*/
+
+	function game() {
+		var guess = parseInt($('#userGuess').val()); //.val gets value of an input field
+		console.log("Yay");
+		if (guess === randomnumber) {
+	    	$('#feedback').html('Congrats! You got it!');	    
+	    }	
+	}
+
 
 });
 
